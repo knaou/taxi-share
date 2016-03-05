8856e44225cca8ebbcfc3c8732f2edc778f84d13
naou <monaou@gmail.com>
2016-03-06 00:17:53 +0900

    凡ミス修正

--- app/models/fee.rb
+++ app/models/fee.rb
@@ -1,5 +1,5 @@
 
 class Fee < ActiveRecord::Base
-  belongs_to :point1, class: 'AreaPoint', foreign_key: :point1_id
-  belongs_to :point2, class: 'AreaPoint', foreign_key: :point2_id
+  belongs_to :point1, class_name: 'AreaPoint', foreign_key: :point1_id
+  belongs_to :point2, class_name: 'AreaPoint', foreign_key: :point2_id
 end
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,8 +1,8 @@
 
 class Requirement < ActiveRecord::Base
-  belongs_to :from_area, class: 'Area', foreign_key: :from_area_id
-  belongs_to :to_area, class: 'Area', foreign_key: :to_area_id
-  belongs_to :from_point, class: 'AreaPoint', foreign_key: :from_point_id
-  belongs_to :to_point, class: 'AreaPoint', foreign_key: :to_point_id
+  belongs_to :from_area, class_name: 'Area', foreign_key: :from_area_id
+  belongs_to :to_area, class_name: 'Area', foreign_key: :to_area_id
+  belongs_to :from_point, class_name: 'AreaPoint', foreign_key: :from_point_id
+  belongs_to :to_point, class_name: 'AreaPoint', foreign_key: :to_point_id
   belongs_to :user
 end
