326cbfc6ee46d928f722b3b09a7cfa21874eec44
naou <monaou@gmail.com>
2016-03-07 01:29:08 +0900

    凡ミス習性

--- app/controllers/areas_controller.rb
+++ app/controllers/areas_controller.rb
@@ -8,6 +8,6 @@ class AreasController < ApplicationController
   def show
     @area = Area.find(params[:id])
     @points = @area.points
-    @requirements = Requirement.where('user_id <> ?', current_user_as_object.id).where(from_area: @area).order('to_area_id ASC')
+    @requirements = Requirement.where(from_area: @area).order('to_area_id ASC')
   end
 end
